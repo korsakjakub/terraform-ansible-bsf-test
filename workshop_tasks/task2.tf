9b717e36d0424d20410879b4839a129c12cc787f
Jakub Korsak <jakub.korsak@bluesoft.com>
2022-08-08 14:55:53 +0200

    fixed random id

--- workshop_tasks/task2.tf
+++ workshop_tasks/task2.tf
@@ -1,10 +1,15 @@
+variable "count_files" {
+  default = 3
+}
+
 resource "random_id" "default" {
   byte_length = 8
+  count = var.count_files
 }
 
 resource "local_file" "task2out" {
-  count = 3
+  count = var.count_files 
   content = templatefile("task2.template", {port="8080", ip_addrs=["0.0.0.0", "8.8.8.8"]})
-  filename = "${path.module}/backend-${random_id.default.hex}"
+  filename = "${path.module}/backend-${element(random_id.default.*.hex, count.index)}"
   file_permission = 0640
 }
